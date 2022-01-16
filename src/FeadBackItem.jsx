9c96490a5d58a57bf4858107f096768ddb886233
Emmanuel Omonzebaguan <emmizy2015@gmail.com>
2022-01-16 16:29:35 +0100

    created facebook login page

---
+++ src/FeadBackItem.jsx
@@ -0,0 +1,10 @@
+function FeadBackItem() {
+    return (
+        <div className='card'>
+            <div className='num-display'>10</div>
+            <div className='text-display'>This is an example of feedback item</div>
+        </div>
+    )
+}
+
+export default FeadBackItem
